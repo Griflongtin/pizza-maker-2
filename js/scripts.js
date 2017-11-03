17ca1036bbeb21fc6e7d724483c59bf045a361fb
Griffin P Longtin <griflongtin@live.com>
2017-11-03 11:57:37 -0700

    change my pizza size prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,42 @@
-function pizza(size, toppings) {
+// BACKEND
+function pizza() {
   this.size = size;
   this.toppings = toppings;
   this.cost = 0;
 }
 
-pizza.prototype.sizeOfPizza = function (size) {
-  if (size = "small") {
-    this.size = "Samll";
-  } else if (size = "medium") {
-    this.size = "Medium";
-  } else if (size = "large") {
-    this.size = "Large";
-  }
+pizza.prototype.sizeOfPizza = function (input) {
+  this.size = input;
 };
 
-pizza.prototype.toppings = function (toppings) {
-  this.toppings = toppings;
+pizza.prototype.toppings = function (input) {
+  this.toppings += input;
 };
+
+pizza.prototype.cost = function () {
+  if(this.size = "Small") {
+    this.cost += 10;
+  } else if (this.size = "Medium") {
+    this.cost += 14;
+  } else if (this.size = "Large") {
+    this.cost += 16;
+  }
+
+  this.toppings.forEach(function(topping) {
+    this.cost += .75;
+  });
+};
+
+
+// FRONT END
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var pizzaSize = $("input:checkbox[name=pizza-size];checked").val();
+
+    });
+  });
+
+
+
+});
