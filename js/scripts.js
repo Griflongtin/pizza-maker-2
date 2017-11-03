5107fa5eac48278f201eeeb8cb72b43b13c2b67e
Griffin P Longtin <griflongtin@live.com>
2017-11-03 11:08:58 -0700

    add a pizza constructor and a pizza prototype for size of pizza

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.cost = 0;
+}
+
+pizza.prototype.sizeOfPizza = function (size) {
+  if (size = "small") {
+    this.size = "Samll";
+  } else if (size = "medium") {
+    this.size = "Medium";
+  } else if (size = "large") {
+    this.size = "Large";
+  }
+};
