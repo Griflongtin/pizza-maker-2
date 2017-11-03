d4747a499a8177c42fd1504d9478fc9e11b61337
Griffin P Longtin <griflongtin@live.com>
2017-11-03 14:26:52 -0700

    add two cost prototypes and will also refactor the code

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,7 @@ function pizza(size, toppings) {
   this.cost = 0;
 }
 
-pizza.prototype.sizeOfPizza = function (input) {
-  this.size = input;
-};
-
-pizza.prototype.toppingsInput = function (input) {
-  this.toppings = input;
-};
-
-pizza.prototype.orderCost = function () {
-  this.toppings.forEach(function(topping) {
-    console.log(topping);
-    this.cost += 1;
-  });
+pizza.prototype.costSize = function () {
   if(this.size = "Small") {
     this.cost += 10;
   } else if (this.size = "Medium") {
@@ -25,22 +13,29 @@ pizza.prototype.orderCost = function () {
   } else if (this.size = "Large") {
     this.cost += 16;
   }
-
 };
 
+pizza.prototype.costTopping = function () {
+  this.cost += .75;
+};
+
+pizza.prototype.orderCost = function () {
+  for (var i = 0; i < this.toppings.length; i++) {
+    this.costTopping();
+  }
+  this.costSize();
+};
 
 // FRONT END
 $(function() {
-  var pizzaTopping = [];
-  var orderPizza = new pizza();
   $("form").submit(function(event) {
     event.preventDefault();
     var pizzaSize = $("#pizza-size").val();
+    var pizzaTopping = [];
     $("input:checkbox[name=pizza-topping]:checked").each(function() {
       pizzaTopping.push($(this).val());
     });
-    orderPizza.sizeOfPizza(pizzaSize);
-    orderPizza.toppingsInput(pizzaTopping);
+    var orderPizza = new pizza(pizzaSize, pizzaTopping);
     debugger
     orderPizza.orderCost();
     console.log(orderPizza);
